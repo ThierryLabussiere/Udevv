f1502badac3fec3204ccebf9b935128842b308ff
ThierryLabussiere <33977129+ThierryLabussiere@users.noreply.github.com>
2018-04-25 12:17:13 +0200

    Update Somme.java

--- Somme.java
+++ Somme.java
@@ -3,15 +3,22 @@ public class Somme {
 	public static void main(String[] args) {
 		int x = 0;
 		int y = 0;
-		int Somme= x + y;;
+		int Somme= x + y;
 		Affiche(Somme);	
+		Affiche(Multiplication);
 		}
 	
 	static void Affiche(int somme) {
 		System.out.println(somme);
 }
-
+	public static int Multiplication (int x, int y) {
+		int c = x*y;
+		return c;
 }
+	public static int Somme (int x, int y) {
+		int c = x+y;
+		return c;
+}}
 
 
 
