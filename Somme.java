3a3932fd36bfa60170fc680991bb69fad4f53b0f
ThierryLabussiere <33977129+ThierryLabussiere@users.noreply.github.com>
2018-04-25 11:51:46 +0200

    Add files via upload

---
+++ Somme.java
@@ -0,0 +1,32 @@
+
+public class Somme {
+	public static void main(String[] args) {
+		int x = 0;
+		int y = 0;
+		int Somme= x + y;;
+		Affiche(Somme);	
+		}
+	
+	static void Affiche(int somme) {
+		System.out.println(somme);
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/*public static void main(String[] args) {
+		int x;
+		int y;
+		int Somme = x + y;
+	    System.out.println(Somme);	}
+ * */
